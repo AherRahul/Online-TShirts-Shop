82e2b8a5330e4dc76c282b0170c4bc87dc3fa975
aherrahul1995 <43370315+aherrahul1995@users.noreply.github.com>
2020-05-11 00:02:31 +0530

    Created User and category schemas

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/T-ShirtDb', {
+mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
@@ -12,7 +13,7 @@ mongoose.connect('mongodb://localhost:27017/T-ShirtDb', {
 })
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send("T-shirt shop");
