9a6930d72265e983caa0ffc8bc656ecca8b490b9
aherrahul1995 <43370315+aherrahul1995@users.noreply.github.com>
2020-05-10 23:52:40 +0530

    DataBase connection Done..!

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/T-ShirtDb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log("DB CONNECTED..!!");
+}).catch(() => {
+    console.log("DB NOT CONNCETED..!!");
+})
 
 const app = express();
-
 const port = 3000;
 
 app.get('/', (req, res) => {
     res.send("T-shirt shop");
 });
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`T-Shit Store app listening at http://localhost:${port}`));
