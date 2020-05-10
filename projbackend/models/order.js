7ed10fb2f957a7cb91b01a8aae5d116dfc5170b9
aherrahul1995 <43370315+aherrahul1995@users.noreply.github.com>
2020-05-10 23:28:42 +0530

    Updated User and category schemas

--- projbackend/models/order.js
+++ projbackend/models/order.js
@@ -1,6 +1,33 @@
 const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema;
 
-const order = new mongoose.Schema({
+const productInCartSchema = new mongoose.Schema({
+    product: {
+        type: ObjectId,
+        ref: "Product"
+    },
+    name: String,
+    count: Number,
+    price: Number
+}, { timestamps: true });
 
-});
+const ProductInCart = mongoose.model("ProductInCart", productInCartSchema);
+
+
+const orderSchema = new mongoose.Schema({
+    products: [productInCartSchema],
+    transaction_id: {},
+    amount: {
+        type: Number
+    },
+    address: String,
+    updated: Date,
+    user: {
+        type: ObjectId,
+        ref: "User"
+    }
+}, { timestamps: true });
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = { Order, ProductInCart };
